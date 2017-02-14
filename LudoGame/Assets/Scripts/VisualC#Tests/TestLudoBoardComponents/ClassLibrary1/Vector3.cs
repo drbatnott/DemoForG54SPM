a1ac62a8e72d28fa89f6bdc57efbe4c1d18f31d2
drbatnott <pxb@cs.nott.ac.uk>
2017-02-14 22:27:29 +0000

    Started Unit Tests

---
+++ LudoGame/Assets/Scripts/VisualC#Tests/TestLudoBoardComponents/ClassLibrary1/Vector3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Vector3
+    {
+        private float x;
+        private float y;
+        private float z;
+
+        public Vector3(float p1, float p2, float p3)
+        {
+            // TODO: Complete member initialization
+            x = p1;
+            y = p2;
+            z = p3;
+        }
+    }
+}
