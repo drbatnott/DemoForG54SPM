7fd66d92035676aae126e335d63ffd2b0d035a6d
drbatnott <pxb@cs.nott.ac.uk>
2017-02-15 21:20:53 +0000

    Added some code for making board and testing square

--- LudoGame/Assets/Scripts/Square.cs
+++ LudoGame/Assets/Scripts/Square.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class Square : MonoBehaviour {
-
+	SquareBehaviour sb;
 	// Use this for initialization
 	void Start () {
-	
+		Transform t = this.GetComponent<Transform>();
+		sb = new SquareBehaviour(t.position);	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void SetPosition(Vector3 v){
+		this.transform.position = v;
 	}
+	public string State { get { return sb.State;} set { sb.State = value; }}
 }
