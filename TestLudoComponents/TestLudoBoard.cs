9195fbf8d25e403c84243bb94f96c615d59646ca
drbatnott <pxb@cs.nott.ac.uk>
2017-02-19 19:50:37 +0000

    Restored all

---
+++ TestLudoComponents/TestLudoBoard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityEngine;
+
+namespace TestLudoComponents
+{
+    [TestClass]
+    public class TestLudoBoard
+    {
+        [TestMethod]
+        public void BoardExits()
+        {
+            LudoGame lg = new LudoGame();
+            
+            Assert.IsNotNull(lg);
+        }
+    }
+}
